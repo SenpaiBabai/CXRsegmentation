07312bad5a02932f12b8128b8eb800bf66b433f6
SenpaiBabai <65128636+SenpaiBabai@users.noreply.github.com>
2020-12-20 10:28:35 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CXRsegmentation-SenpaiBabai", 
+    version="0.0.1",
+    author="CXR_team",
+    author_email="dkhasanov76@gmail.com",
+    description="Проект по сегментации легких.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SenpaiBabai/CXRsegmentation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License, Version 2.0",
+        "Operating System :: Windows 10 / Linux (Ubuntu)",
+    ],
+    python_requires='>=3.6.9',
+)
